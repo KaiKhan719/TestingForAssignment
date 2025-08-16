59d67a50e8c2ab995c3d34ac66e2bdd58596027a
Kai Yuen Cheung <134359367+KaiKhan719@users.noreply.github.com>
2025-08-16 13:27:58 +0800

    Update test.py

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ print("hello")
 print("My name is Vansh")
 
 print("please work")
-print("fuck this app")print("fuck this app")
+print("fuck this app")
 =======
 print("what")
 >>>>>>> 4b1c3bc509ed98fc3ea8fec2a230b71628f266ef
